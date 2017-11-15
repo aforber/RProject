ab9d51b5c706f1a5d9ec8081f1f261d1782395a7
Alyssa Forber <alyssa.forber@ucdenver.edu>
2017-11-14 17:52:31 -0700

    Created district column and full data column

--- ProjCode.R
+++ ProjCode.R
@@ -19,55 +19,45 @@ library(rgdal)
 
 inter <- read.csv('/Users/alyssaforber/Documents/Denver/Fall2017/RPython/intervention.csv', header=T)
 incid <- read.csv('/Users/alyssaforber/Documents/Denver/Fall2017/RPython/incidence.csv')
-
 poly <- readShapePoly('/Users/alyssaforber/Documents/Denver/Fall2017/RPython/Moz_admin2.shp', IDvar="DISTCODE")
-
-# this is what they look like individually
-zam <- read.table("http://rap.ucar.edu/staff/monaghan/colborn/mozambique/daily/v2/ALTO_MOLOCUE_ZAM_fldas_daily_20100101-20170724.txt",
-                  skip=3, header = FALSE, sep ='', stringsAsFactors = FALSE, dec=".")
-colnames(zam) <- c("year", "mo", "day", "raint", "tavg", "rh", "sd", "psfc")
-
-#http://rap.ucar.edu/staff/monaghan/colborn/mozambique/daily/v2/
-
-# reading this in is not straight forwards
-#zam2 <- read.table("http://rap.ucar.edu/staff/monaghan/colborn/mozambique/daily/v2/")
-
-
-# This creates the file paths! 
-# we just need to extract a list of the district names
-
-# PUT TOGETHER THE FILE PATHS FROM A LIST OF DISTRICT NAMES
-g <-file.path("https:/", c("district1", "district2", "district3"))
-h <- file.path(g, "fldas_daily_20100101-20170724.txt", fsep= "_")
 
 
 
 # code from https://stackoverflow.com/questions/15954463/read-list-of-file-names-from-web-into-r
+
 # EXTRACT ALL THE LINKS FOR EACH FILE
 library(XML)
 url <- "http://rap.ucar.edu/staff/monaghan/colborn/mozambique/daily/v2/"
 doc <- htmlParse(url)
 links <- xpathSApply(doc, "//a/@href")
 free(doc)
-links
-
-
 wanted <- links[grepl("*_fldas_daily_20100101-20170724.txt", links)]
-wanted <- links[grepl("Amelia_1\\.2.*", links)]
 GetMe <- paste(url, wanted, sep = "")
 
-# FUNCTION THAT READS TABLE CORRECTLY
-myread <- function(f){
-  read.table("f", skip=3, header = FALSE, sep ='', stringsAsFactors = FALSE, dec=".")
-}
 
-# get a list of data frames
+# LIST OF DATA FRAMES
 d <- lapply(seq_along(GetMe), 
             function(x) read.table(GetMe[1], skip=3, header = FALSE, sep ='', stringsAsFactors = FALSE, dec="."))
 
+# ADD COLUMN NAMES
 for (i in seq_along(d)){
   colnames(d[[i]]) <- c("year", "mo", "day", "raint", "tavg", "rh", "sd", "psfc")
 }
+
+# GET LIST OF DISTRICT NAMES
+districts <- as.list(gsub( "_.{3}_fldas_daily_20100101-20170724.txt", "", links ))
+districts <- gsub("_", " ", districts)
+districts <- districts[6:length(districts)]
+
+
+# ADD COLUMN FOR DISTRICT NAMES
+d <- Map(cbind, d, District = districts)
+
+# COMBINE DATE
+for (i in seq_along(d)){
+  d[[i]]$longDate <- as.Date(paste(d[[i]]$year, d[[i]]$mo, d[[i]]$day, sep = "/"), format = "%Y/%m/%d")
+}
+
 
 
 # package called epiweek
